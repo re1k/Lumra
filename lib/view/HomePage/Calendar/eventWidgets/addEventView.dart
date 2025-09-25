265d8f38b874ce6dc33198e0c185ad5b9240d776
re1k <reem.kd78@gmail.com>
2025-09-25 18:21:44 +0300

    AA

--- lib/view/HomePage/Calendar/eventWidgets/addEventView.dart
+++ lib/view/HomePage/Calendar/eventWidgets/addEventView.dart
@@ -20,9 +20,6 @@ class AddEventView extends StatelessWidget {
         authContoller.currentUser!.uid,
       ),
     );
-
-      final IsControllerAdded = controller.isEventAdded; // just assign the reactive var
-
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -39,211 +36,238 @@ class AddEventView extends StatelessWidget {
       ),
 
       body: Padding(
-        padding: const EdgeInsets.all(
-          BSizes.defaultSpace,
+        padding: EdgeInsets.only(
+          left: BSizes.defaultSpace,
+          right: BSizes.defaultSpace,
+          top: BSizes.defaultSpace,
+          bottom: BSizes.sm,
         ), // clear margin around content
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // ------------------EVENT TITLE------------------ //
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // ------------------EVENT TITLE------------------ //
 
-            //THE LABEL
-            Text("Title", style: BTextTheme.lightTextTheme.titleSmall),
+              //THE LABEL
+              Text("Title", style: BTextTheme.lightTextTheme.titleSmall),
 
-            // Distance between the label and the feild
-            const SizedBox(height: 8),
+              // Distance between the label and the feild
+              const SizedBox(height: 8),
 
-            //THE FEILD
-            Obx(() {
-              final hasError =
-                  controller.titleError.value != null &&
-                  controller.titleFieldTouched.value;
+              //THE FEILD
+              Obx(() {
+                final hasError =
+                    controller.titleError.value != null &&
+                    controller.titleFieldTouched.value;
 
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextField(
-                    controller: controller.titleController,
-                    // this method i made in my ontroller to watch out for input validity for turning red
-                    onChanged: controller.updateTitle,
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextField(
+                      controller: controller.titleController,
+                      // this method i made in my ontroller to watch out for input validity for turning red
+                      onChanged: controller.updateTitle,
 
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                          BSizes.inputFieldRadius,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                            BSizes.inputFieldRadius,
+                          ),
+                          borderSide: BorderSide(
+                            color: controller.titleError.value != null
+                                ? BColors.error
+                                : BColors.darkGrey,
+                          ),
                         ),
-                        borderSide: BorderSide(
-                          color: controller.titleError.value != null
-                              ? BColors.error
-                              : BColors.darkGrey,
-                        ),
-                      ),
 
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                          BSizes.inputFieldRadius,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                            BSizes.inputFieldRadius,
+                          ),
+                          borderSide: BorderSide(
+                            color: controller.titleError.value != null
+                                ? BColors.error
+                                : BColors.darkGrey,
+                          ),
                         ),
-                        borderSide: BorderSide(
-                          color: controller.titleError.value != null
-                              ? BColors.error
-                              : BColors.darkGrey,
-                        ),
-                      ),
 
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(
-                          BSizes.inputFieldRadius,
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                            BSizes.inputFieldRadius,
+                          ),
+                          borderSide: BorderSide(
+                            color: controller.titleError.value != null
+                                ? BColors.error
+                                : BColors.darkGrey,
+                            width: 1.3,
+                          ),
                         ),
-                        borderSide: BorderSide(
-                          color: controller.titleError.value != null
-                              ? BColors.error
-                              : BColors.darkGrey,
-                          width: 1.3,
-                        ),
-                      ),
-                      errorText: null, // remove error text if correct feild
-                      contentPadding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                        vertical: 16,
-                      ),
-                    ),
-                  ),
-
-                  // Error message below the field
-                  if (hasError)
-                    Padding(
-                      padding: const EdgeInsets.only(right: 0, top: 4, left: 8),
-                      child: Text(
-                        controller.titleError.value!,
-                        style: const TextStyle(
-                          color: BColors.error,
-                          fontSize: 14,
+                        errorText: null, // remove error text if correct feild
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                          vertical: 16,
                         ),
                       ),
                     ),
-                ],
-              );
-            }),
 
-            const SizedBox(height: BSizes.spaceBtwinputFields),
-
-            // ------------------ Start Time ------------------ //
-            Text("Start time", style: BTextTheme.lightTextTheme.titleSmall),
-            const SizedBox(height: 8),
-
-            //Obx is from GetX. It automatically rebuilds this widget whenever the reactive variable changes (eventStart).
-            Obx(() {
-              final startTimestamp = controller.eventStart.value;
-              //this is just for UI, to make time human readable not timeStamp
-              final startText = startTimestamp != null
-                  ? TimeOfDay.fromDateTime(
-                      startTimestamp.toDate(),
-                    ).format(Get.context!)
-                  : 'Select Start Time';
-
-              return GestureDetector(
-                //To show the TimePicker when user Clicks
-                onTap: () => controller.pickTime(isStart: true),
-
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 24,
-                    vertical: 16,
-                  ),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: BColors.darkGrey), // border color
-                    borderRadius: BorderRadius.circular(
-                      BSizes.inputFieldRadius,
-                    ),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [Text(startText), const Icon(Icons.access_time)],
-                  ),
-                ),
-              );
-            }),
-
-            const SizedBox(height: BSizes.SpaceBtwItems),
-
-            // ------------------ End Time ------------------ //
-            Text("End time", style: BTextTheme.lightTextTheme.titleSmall),
-            const SizedBox(height: 8),
-
-            Obx(() {
-              final endTimestamp = controller.eventEnd.value;
-              final endText = endTimestamp != null
-                  ? TimeOfDay.fromDateTime(
-                      endTimestamp.toDate(),
-                    ).format(Get.context!)
-                  : 'Select End Time';
-              final hasError = controller.endError.value != null;
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  GestureDetector(
-                    onTap: () => controller.pickTime(isStart: false),
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 24,
-                        vertical: 16,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: hasError ? BColors.error : BColors.darkGrey,
+                    // Error message below the field
+                    if (hasError)
+                      Padding(
+                        padding: const EdgeInsets.only(
+                          right: 0,
+                          top: 4,
+                          left: 8,
                         ),
-                        borderRadius: BorderRadius.circular(
-                          BSizes.inputFieldRadius,
+                        child: Text(
+                          controller.titleError.value!,
+                          style: const TextStyle(
+                            color: BColors.error,
+                            fontSize: 14,
+                          ),
                         ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(endText),
-                          const Icon(Icons.access_time),
-                        ],
+                  ],
+                );
+              }),
+
+              const SizedBox(height: BSizes.spaceBtwinputFields),
+
+              // ------------------ Start Time ------------------ //
+              Text("Start time", style: BTextTheme.lightTextTheme.titleSmall),
+              const SizedBox(height: 8),
+
+              //Obx is from GetX. It automatically rebuilds this widget whenever the reactive variable changes (eventStart).
+              Obx(() {
+                final startTimestamp = controller.eventStart.value;
+                //this is just for UI, to make time human readable not timeStamp
+                final startText = startTimestamp != null
+                    ? TimeOfDay.fromDateTime(
+                        startTimestamp.toDate(),
+                      ).format(Get.context!)
+                    : 'Select Start Time';
+
+                return GestureDetector(
+                  //To show the TimePicker when user Clicks
+                  onTap: () => controller.pickTime(isStart: true),
+
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: BColors.darkGrey,
+                      ), // border color
+                      borderRadius: BorderRadius.circular(
+                        BSizes.inputFieldRadius,
                       ),
                     ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(startText),
+                        const Icon(Icons.access_time),
+                      ],
+                    ),
                   ),
+                );
+              }),
 
-                  // Error message below the field
-                  if (hasError)
-                    Padding(
-                      padding: const EdgeInsets.only(top: 4, left: 8),
-                      child: Text(
-                        controller.endError.value!,
-                        style: const TextStyle(
-                          color: BColors.error,
-                          fontSize: 14,
+              const SizedBox(height: BSizes.SpaceBtwItems),
+
+              // ------------------ End Time ------------------ //
+              Text("End time", style: BTextTheme.lightTextTheme.titleSmall),
+              const SizedBox(height: 8),
+
+              Obx(() {
+                final endTimestamp = controller.eventEnd.value;
+                final endText = endTimestamp != null
+                    ? TimeOfDay.fromDateTime(
+                        endTimestamp.toDate(),
+                      ).format(Get.context!)
+                    : 'Select End Time';
+                final hasError = controller.endError.value != null;
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    GestureDetector(
+                      onTap: () => controller.pickTime(isStart: false),
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 24,
+                          vertical: 16,
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: hasError ? BColors.error : BColors.darkGrey,
+                          ),
+                          borderRadius: BorderRadius.circular(
+                            BSizes.inputFieldRadius,
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(endText),
+                            const Icon(Icons.access_time),
+                          ],
                         ),
                       ),
                     ),
-                ],
-              );
-            }),
 
-            const SizedBox(height: BSizes.appBarHeight),
+                    // Error message below the field
+                    if (hasError)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 4, left: 8),
+                        child: Text(
+                          controller.endError.value!,
+                          style: const TextStyle(
+                            color: BColors.error,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                  ],
+                );
+              }),
 
-            // ------------------ ADD BUTTON ------------------ //
-            
-            Obx(
-              () => SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  onPressed: controller.isFormValid.value
-                      ? () => controller.addEventToFirebase()
-                      : null, // null disables the button
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 14),
-                  ),
-                  child: Text(
-                    "Save",
-                    style: BTextTheme.darkTextTheme.headlineSmall,
+              const SizedBox(height: BSizes.appBarHeight),
+
+              // ------------------ ADD BUTTON ------------------ //
+              Obx(
+                () => SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: controller.isFormValid.value
+                        ? () async {
+                            // Make sure validation runs one last time
+                            if (controller.validateForm()) {
+                              await controller.addEventToFirebase();
+                              Navigator.pop(context); // closes the view
+                            }
+                          }
+                        : null, // disables button if form not valid // null disables the button
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(vertical: 14),
+                    ),
+
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Icon(Icons.check),
+                        const SizedBox(width: 8),
+                        Text(
+                          "Add",
+                          style: BTextTheme.darkTextTheme.headlineSmall,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
