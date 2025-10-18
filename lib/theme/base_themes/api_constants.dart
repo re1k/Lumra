873bf12973ed442651dede2744e7944625691500
byremaz <byremaz@gmail.com>
2025-10-19 01:26:06 +0300

    update

--- lib/theme/base_themes/api_constants.dart
+++ lib/theme/base_themes/api_constants.dart
@@ -4,9 +4,5 @@ library;
 class APIsConstants {
   static const String bSecretAPIKey = '';
 
-<<<<<<< HEAD
-  static const String Gimini_API = "AIzaSyAyQRtNlFOi4tH5EllGRbzfNvd0wGUnRLk";
-=======
   static const String Gimini_API = "AIzaSyBmuNZhOiEE17onwdrtaDvs9QlWzN-zSTQ";
->>>>>>> e7852ad3407098f28c626fab961675fa70691c1f
 }
