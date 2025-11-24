6ae9e7972f70285096ecda8c01eaa4fc74c3ad47
1l2a <latifabhafid370@gmail.com>
2025-11-24 12:34:43 +0300

    edit profile

--- lib/view/Account/ViewProfile.dart
+++ lib/view/Account/ViewProfile.dart
@@ -20,7 +20,6 @@ class ViewProfile extends StatelessWidget {
     }
 
     authController = Get.find<AuthController>();
-<<<<<<< HEAD
 
     final currentUser = userController.user.value;
     if (currentUser != null) {
@@ -32,8 +31,6 @@ class ViewProfile extends StatelessWidget {
     }
     
     isEditing.value = false;
-=======
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
   }
 
   late final UserController userController;
@@ -43,19 +40,20 @@ class ViewProfile extends StatelessWidget {
   final firstNameError = ''.obs;
   final lastNameError = ''.obs;
 
-<<<<<<< HEAD
   
   final _formKey = GlobalKey<FormState>();
   final RxBool _triggerRebuild = false.obs;
 
-=======
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
   Future<void> _selectDateOfBirth(BuildContext context) async {
+   
+    final userRole = userController.role.value; 
+    final int lastYear = (userRole == 'caregiver') ? 2007 : 2019;
+
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: userController.dob.value,
       firstDate: DateTime(1955, 1, 1),
-      lastDate: DateTime(2019, 12, 31),
+      lastDate: DateTime(lastYear, 12, 31),
       builder: (context, child) {
         return Theme(
           data: Theme.of(context).copyWith(
@@ -79,12 +77,9 @@ class ViewProfile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Obx(() {
       final user = userController.user.value;
-<<<<<<< HEAD
       
       final refresh = _triggerRebuild.value; 
 
-=======
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
       if (user == null) return const Center(child: CircularProgressIndicator());
 
       return Scaffold(
@@ -96,26 +91,18 @@ class ViewProfile extends StatelessWidget {
               context: context,
               title: 'Profile',
               showBackButton: true,
-<<<<<<< HEAD
               onBackPressed: () async {
                 if (await _onWillPop(context)) {
                   Navigator.pop(context);
                 }
               },
               actions: [
-=======
-              onBackPressed: () => Navigator.pop(context),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
                 Padding(
                   padding: const EdgeInsets.only(right: 16.0),
-                  child: Center( // 1. وضعنا Center لكي لا يمتط الزر
+                  child: Center(
                     child: Container(
-                      width: 45, // 2. ثبتنا العرض
-                      height: 50, // 3. ثبتنا الطول ليكون نفس حجم القديم
+                      width: 45,
+                      height: 50,
                       decoration: BoxDecoration(
                         color: BColors.white,
                         borderRadius: BorderRadius.circular(8),
@@ -129,7 +116,7 @@ class ViewProfile extends StatelessWidget {
                       ),
                       child: IconButton(
                         tooltip: 'edit profile',
-                        padding: EdgeInsets.zero, // إزالة الحواف الداخلية للأيقونة
+                        padding: EdgeInsets.zero,
                         icon: const Icon(
                           Icons.edit,
                           color: BColors.primary,
@@ -145,7 +132,7 @@ class ViewProfile extends StatelessWidget {
               ],
             ),
             
-            // قمنا بحذف الـ Row المنفصلة التي كانت هنا سابقاً
+          
             
             Expanded(
               child: SingleChildScrollView(
@@ -163,26 +150,19 @@ class ViewProfile extends StatelessWidget {
                         label: "First Name",
                         controller: userController.firstNameController,
                         errorText: firstNameError,
+                        onChanged: (_) => _triggerRebuild.toggle(),
                       ),
                       _buildTextField(
                         label: "Last Name",
                         controller: userController.lastNameController,
-                          errorText: lastNameError,
+                        errorText: lastNameError,
+                        onChanged: (_) => _triggerRebuild.toggle(), //  
                       ),
-<<<<<<< HEAD
                       
                         _buildEmailField(
                           label: "Email",
                           controller: userController.emailController,
                         ),
-=======
-
-                      if(!isEditing.value)
-                      _buildEmailField(
-                        label: "Email",
-                        controller: userController.emailController,
-                      )  ,
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
                       _buildGenderField(),
                       const SizedBox(height: 10),
                       _buildDobField(
@@ -192,21 +172,14 @@ class ViewProfile extends StatelessWidget {
                       ),
                       const SizedBox(height: 20),
                       Obx(() {
-                        return isEditing.value
-                            ? SizedBox(
-                                width: double.infinity,
-                                child: ElevatedButton.icon(
-                                  onPressed: () {
-                                     bool valid = true;
+                        if (!isEditing.value) return const SizedBox();
 
-                                    if (userController.firstNameController.text.trim().isEmpty) {
-                                    firstNameError.value = "Should not be empty";
-                                    valid = false;
-                                      } else {
-                                       firstNameError.value = '';
-                                       }
+                        bool hasChanges = 
+                            userController.firstNameController.text != user.firstName ||
+                            userController.lastNameController.text != user.lastName ||
+                            userController.gender.value != user.gender ||
+                            userController.dob.value != user.dob;
 
-<<<<<<< HEAD
                         
                         bool isValidInput = 
                             userController.firstNameController.text.trim().isNotEmpty &&
@@ -235,40 +208,15 @@ class ViewProfile extends StatelessWidget {
                                 padding: const EdgeInsets.symmetric(
                                   vertical: 8,
                                   horizontal: 0,
-=======
-                                    if (userController.lastNameController.text.trim().isEmpty) {
-                                     lastNameError.value = "Should not be empty";
-                                     valid = false;
-                                      } else {
-                                        lastNameError.value = '';
-                                        }
-                             
-                                   if (valid) {
-                                    userController.updateUserFromControllers();
-                                    _showConfiramtinMessage(context);
-                                    isEditing.value = false;
-
-                                       }
-                                        },
-
-                                  icon: const Icon(Icons.save),
-                                  label: const Text("Save"),
-                                  style: ElevatedButton.styleFrom(
-                                    backgroundColor: BColors.primary,
-                                    foregroundColor: BColors.textwhite,
-                                    padding: const EdgeInsets.symmetric(
-                                      vertical: 15,
-                                      horizontal: 0,
-                                    ),
-                                    textStyle: BTextTheme.lightTextTheme.headlineSmall,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(16),
-                                    ),
-                                  ),
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
                                 ),
-                              )
-                            : const SizedBox();
+                                textStyle: BTextTheme.lightTextTheme.headlineSmall,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(16),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
                       }),
                     ],
                   ),
@@ -281,7 +229,6 @@ class ViewProfile extends StatelessWidget {
     });
   }
 
-<<<<<<< HEAD
  Future<bool> _onWillPop(BuildContext context) async {
 
     if (!isEditing.value) {
@@ -355,9 +302,6 @@ class ViewProfile extends StatelessWidget {
   }
 
   void _showConfiramtinMessage(BuildContext context) {
-=======
-   void _showConfiramtinMessage(BuildContext context) {
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
     showDialog(
       context: context,
       barrierDismissible: false,
@@ -380,7 +324,7 @@ class ViewProfile extends StatelessWidget {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-               Text(
+              Text(
                 "Profile Updated",
                 textAlign: TextAlign.left,
                 style: const TextStyle(
@@ -390,7 +334,6 @@ class ViewProfile extends StatelessWidget {
                   color: BColors.primary,
                 ),
               ),
-             
               const SizedBox(height: 16),
               const Text(
                 "The profile was updated successfully.",
@@ -427,7 +370,6 @@ class ViewProfile extends StatelessWidget {
     );
   }
 
-<<<<<<< HEAD
   Widget _buildTextField({
     required String label,
     required TextEditingController controller,
@@ -487,7 +429,7 @@ class ViewProfile extends StatelessWidget {
     );
   }
 
-  // 2. التعديل الثاني: تغيير لون الخلفية للإيميل
+ 
   Widget _buildEmailField({
     required String label,
     required TextEditingController controller,
@@ -498,20 +440,20 @@ class ViewProfile extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // وضعنا العنوان والنص في Row ليكونوا بجانب بعضهم
+         
           Row(
-            crossAxisAlignment: CrossAxisAlignment.end, // لمحاذاة النص على نفس السطر من الأسفل
+            crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Text(label, style: BTextTheme.lightTextTheme.titleSmall),
               
               if (isEditing.value)
                 Padding(
-                  padding: const EdgeInsets.only(left: 6.0, bottom: 2.0), // مسافة صغيرة عن العنوان
+                  padding: const EdgeInsets.only(left: 6.0, bottom: 2.0),
                   child: Text(
                     "Not editable",
                     style: TextStyle(
-                      color: Colors.grey.shade600, // لون رمادي كما في الصورة
-                      fontSize: 11, // خط صغير
+                      color: Colors.grey.shade600,
+                      fontSize: 11,
                       fontWeight: FontWeight.normal,
                     ),
                   ),
@@ -519,7 +461,7 @@ class ViewProfile extends StatelessWidget {
             ],
           ),
           
-          const SizedBox(height: 8), // مسافة بين العنوان والحقل
+          const SizedBox(height: 8),
 
           Obx(
             () => TextField(
@@ -527,93 +469,19 @@ class ViewProfile extends StatelessWidget {
               readOnly: true,
               decoration: InputDecoration(
                 filled: true,
-                // تغميق اللون عند التعديل
+               
                 fillColor: isEditing.value 
                     ? Colors.grey.shade300 
                     : BColors.softGrey,
                 border: const OutlineInputBorder(),
                 contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-=======
-
-
-
-  Widget _buildTextField({
-  required String label,
-  required TextEditingController controller,
-  IconData? icon,
-  RxString? errorText,
-}) {
-  return Padding(
-    padding: const EdgeInsets.only(bottom: 15),
-    child: Obx(() {
-      return Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(label, style: BTextTheme.lightTextTheme.titleSmall),
-          
-         
-          if (errorText != null && errorText.value.isNotEmpty)
-            Padding(
-              padding: const EdgeInsets.only(top: 2, bottom: 4),
-              child: Text(
-                errorText.value,
-                style: const TextStyle(color: Colors.red, fontSize: 12),
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
               ),
-            ),
-
-          TextField(
-            controller: controller,
-            readOnly: !isEditing.value,
-            maxLength: 12,
-            inputFormatters: [
-              FilteringTextInputFormatter.allow(RegExp(r'[A-Za-z]')),
-            ],
-            decoration: InputDecoration(
-              counterText: isEditing.value ? null : '',
-              filled: true,
-              fillColor: BColors.softGrey,
-              prefixIcon: icon != null ? Icon(icon) : null,
-              border: const OutlineInputBorder(),
-              contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
             ),
           ),
         ],
-      );
-    }),
-  );
-}
-
-
-Widget _buildEmailField({
-  required String label,
-  required TextEditingController controller,
-  IconData? icon,
-}) {
-  return Padding(
-    padding: const EdgeInsets.only(bottom: 15),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(label, style: BTextTheme.lightTextTheme.titleSmall),
-        const SizedBox(height: 8),
-        Obx(
-          () => TextField(
-            controller: controller,
-            readOnly: !isenable.value , 
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: BColors.softGrey,
-              
-              border: const OutlineInputBorder(),
-              contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
-}
+      ),
+    );
+  }
 
   Widget _buildDobField({
     required BuildContext context,
@@ -637,13 +505,8 @@ Widget _buildEmailField({
               decoration: InputDecoration(
                 filled: true,
                 fillColor: BColors.softGrey,
-<<<<<<< HEAD
                 
                 suffixIcon: icon != null ? Icon(icon) : null, 
-=======
-                prefixIcon: icon != null ? Icon(icon) : null,
-                
->>>>>>> 859f9d2ff5c9708e6846e85e5c2453a2f6b89461
                 hintText: 'YYYY-MM-DD',
                 border: const OutlineInputBorder(),
                 contentPadding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
@@ -655,15 +518,13 @@ Widget _buildEmailField({
     );
   }
 
-
- Widget _buildGenderField() {
+  Widget _buildGenderField() {
     return Obx(() {
       final gender = userController.gender.value.toLowerCase();
 
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // const Text("Gender", style: BTextTheme.lightTextTheme.titleSmall ),
           Text("Gender", style: BTextTheme.lightTextTheme.titleSmall),
           const SizedBox(height: 2),
           Row(
@@ -671,8 +532,7 @@ Widget _buildEmailField({
               Expanded(
                 child: ElevatedButton.icon(
                   onPressed: () {
-                    if(isEditing.value)
-                    userController.gender.value = 'male';
+                    if (isEditing.value) userController.gender.value = 'male';
                   },
                   icon: const Icon(Icons.boy, size: 25),
                   label: const Text("Male"),
@@ -695,8 +555,7 @@ Widget _buildEmailField({
               Expanded(
                 child: ElevatedButton.icon(
                   onPressed: () {
-                    if(isEditing.value)
-                    userController.gender.value = 'female';
+                    if (isEditing.value) userController.gender.value = 'female';
                   },
                   icon: const Icon(Icons.girl, size: 25),
                   label: const Text("Female"),
@@ -721,7 +580,4 @@ Widget _buildEmailField({
       );
     });
   }
-
-
-
 }
